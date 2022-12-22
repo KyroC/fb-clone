0b755bd55944e2369f195d72069bc1b139a53a7b
KyroC <kyro.eth@gmail.com>
2022-12-22 19:42:38 +0800

    added navbar css + icons

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './navbar.css';
-import Logo from '../fb-logo.svg'
+import Logo from '../fb-logo.svg';
+import Search from '../search-icon.svg';
+import Home from '../home_icon.svg';
+import Thumb from '../thumb_icon.svg';
+import Profile from '../profile_icon.svg'
 
 export default function Navbar() {
 
@@ -9,15 +13,15 @@ export default function Navbar() {
         <div className="navbar">
             <div className="left_nav">
                 <div className="logo"><img src={Logo} /></div>
-                <div className="search"></div>
+                <div className="search"><img src={Search} /></div>
             </div>
             <div className="middle_nav">
-                <div className="home"></div>
-                <div className="likes"></div>
+                <div className="home"><img src={Home} /></div>
+                <div className="likes"><img src={Thumb} /></div>
             </div>
             <div className="right_nav">
                 <div className="chat"></div>
-                <div className="profile"></div>
+                <div className="profile"><img src={Profile} /></div>
             </div>
         </div>
     )
