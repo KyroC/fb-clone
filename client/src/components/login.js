bec5b367c70d0cf217fd0b1d6ae1a391ba902412
KyroC <kyro.eth@gmail.com>
2022-12-28 17:30:48 +0800

    updated login

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,4 +1,4 @@
-import {React, useState} from 'react';
+import React, {useState} from 'react';
 import "./login.css";
 import Logo from "../fb-word-logo.svg";
 import { Link, useNavigate } from "react-router-dom";
